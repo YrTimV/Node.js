c6a35954da07b8e08ca3b6e65865bb26409668a9
Yury Timofeev <IT.YrTimV@GMail.Com>
2018-04-24 08:37:01 +0300

    Fixed the use of newsCount param during news fetching.

--- Lesson 03/news_reader/index.js
+++ Lesson 03/news_reader/index.js
@@ -2,13 +2,12 @@ const request = require('request');
 const clc = require('cli-color');
 const cli = require('cli');
 const cliOptions = {
-	newsCount: ['n', 'Fetch first NUM of news.', 'num']
+	newsCount: ['n', 'Fetch first NUM of news.', 'num', 10]
 };
 
 cli.setApp('A simple news reader app.', '0.0.1');
 cli.enable('version');
 cli.parse(cliOptions);
-cli.options.newsCount = cli.options.newsCount || 10;
 	
 request('https://www.vesti.ru/news/', (err, res, body) => {
 	if (err) {
@@ -28,17 +27,16 @@ request('https://www.vesti.ru/news/', (err, res, body) => {
 	
 	const $ = require('cheerio').load(body);
 	const $newsItems = $('.b-item__inner', '.news-wrapper_');
-	const newsCountToFetch = 10;
 	let $newsInnerItem, $newsItem, newsTitle, newsText, newsTime;
 	
-	for (let i = 0; (($newsItem = $newsItems[i]) && i < newsCountToFetch); i++) {
+	for (let i = 0; (($newsItem = $newsItems[i]) && i < cli.options.newsCount); i++) {
 		$newsInnerItem = $('.b-item__title a', $newsItem);
-		newsTitle = $newsInnerItem[0].children[0].nodeValue.trim();
+		newsTitle = $newsInnerItem.text().trim();
 		$newsInnerItem = $('.b-item__text', $newsItem);
 		newsText = ($newsInnerItem.length ?
-						$newsInnerItem[0].children[0].nodeValue.trim() : '');
+						$newsInnerItem.text().trim() : '');
 		$newsInnerItem = $('.b-item__time', $newsItem);
-		newsTime = $newsInnerItem[0].children[0].nodeValue.trim();
+		newsTime = $newsInnerItem.text().trim();
 
 		console.log('\nNews #' + clc.bold.magentaBright(i + 1) + ' (' +
 						clc.bold.cyanBright(newsTime) + '): ' + clc.bold.yellowBright(newsTitle) +
